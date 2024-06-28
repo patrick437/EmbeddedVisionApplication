5396a2db5c435be7af94b160e8979392cd03c0a2
patrick437 <patrickgnoonan@gmail.com>
2024-06-28 12:44:25 +0100

    Doing 2 cameras in a more efficient way

--- FaceDetection.py
+++ FaceDetection.py
@@ -6,38 +6,28 @@ from  picamera2 import Picamera2
 
 def createCamera():
 	cv2.startWindowThread()
-
-	picam1 = Picamera2()
-	picam2 = Picamera2()
-
-	picam2.configure(picam1.create_preview_configuration(main={"format":"XRGB8888","size": (640,480)}))
-	picam2.configure(picam2.create_preview_configuration(main={"format":"XRGB8888","size": (640,480)}))
-
-	picam1.start()
-	picam2.start()
-
-	return picam1, picam2
+	picam = Picamera2()
+	picam.configure(picam1.create_preview_configuration(main={"format":"XRGB8888","size": (640,480)}))
+	picam.start()
+	return picam
 
 def captureImageArrays(picam1, picam2):
 	im1 = picam1.capture_array()
-	im2 = picam2.capture_array()
+	grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+	return im
 
-	grey1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-	grey2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+def iterateThroughData(im):
 
-	return im1, im2
-
-def iterateThroughData(im1, im2):
-
-	print( len(im1) )
-	print( len(im2) )
+	print( len(im) )
+	
 
 	
 def main():
 	
-	picam1, picam2 = createCamera()
-
-	im1, im2 = captureImageArrays(picam1, picam2)
+	picam1 = createCamera()
+	picam2 = createCamera()
+	im1 = captureImageArrays(picam1)
+	im2 = captureImageArrays(picam2)
 
 	cv2.imshow("Camera", im1)
 	cv2.imshow("Camera2", im2)	
